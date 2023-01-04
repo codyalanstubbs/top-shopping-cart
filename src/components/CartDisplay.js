3d2f46b138946acce73663a934e8bda35c8ff8f9
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2023-01-04 09:52:35 -0500

    Style and structure products in cart

--- src/components/CartDisplay.js
+++ src/components/CartDisplay.js
@@ -3,32 +3,27 @@ import "../assets/css/CartDisplay.css";
 import FlavorWheelImage from "../assets/images/flavor-wheel.png";
 
 
-const CartDisplay = ({shoppingCart, toggleDisplayCart,  increaseQtyInCart, decreaseQtyInCart}) => {
+const CartDisplay = ({shoppingCart, toggleDisplayCart,  increaseQtyInCart, decreaseQtyInCart, setQtyInCart}) => {
 
     return (
         <div className="displayed-cart" data-testid="displayed-cart">
             <div className="close-cart" onClick={toggleDisplayCart}>{'Hide>'}</div>
+            <h2>Shopping Cart</h2>
             {
                 shoppingCart.map((product) => {
                     const { id, name, cost, qty } = product;
-                        
-                    const handleIncreaseQty = () => {
-                        increaseQtyInCart(product);
-                    };
-
-                    const handleDecreaseQty = () => {
-                        decreaseQtyInCart(id);
-                    };
-
                     return (
                         <div key={id} id={id} className="product-box" data-testid="product-box">
                             <img src={FlavorWheelImage} alt="A flavor wheel for green tea." width="100px" height="100px" />
-                            <h2 data-testid="p-name">{name} Lexicon</h2>
-                            <p data-testid="p-cost">{"$"+cost}</p>
-                            <div className="p-qty-container">
-                                <button className="qty-adjuster" onClick={handleDecreaseQty}>-</button>
-                                <input data-testid="p-qty" value={qty} onChange={() => console.log("do nothing")}/>
-                                <button className="qty-adjuster" onClick={handleIncreaseQty}>+</button>
+                            <div className="product-details">
+                                <div className="lexicon-cost">
+                                    <h3 data-testid="p-name">{name} Lexicon</h3>
+                                    <p data-testid="p-cost">{"$"+cost}</p>
+                                </div>
+                                <div className="qty-container">
+                                    <p>Qty:&nbsp;</p>
+                                    <p data-testid="p-qty" className="cart-qty">{qty}</p>
+                                </div>
                             </div>
                         </div>
                     );
